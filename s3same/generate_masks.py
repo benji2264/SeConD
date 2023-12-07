6b34190858a23e06227c07a676ecc7229b3d52d7
Benjamin Missaoui <benji2264@gmail.com>
2023-12-07 14:35:50 +0800

    utils

--- s3same/generate_masks.py
+++ s3same/generate_masks.py
@@ -8,7 +8,7 @@ import numpy as np
 import pandas as pd
 import torch
 
-from utils import *
+from sam_utils import *
 
 random.seed(0)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
